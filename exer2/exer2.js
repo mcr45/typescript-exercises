fd019d07746638f3f48bf2191345c4b667d4cccc
mcr45 <depr3604@protonmail.com>
2023-09-24 11:22:36 -0500

    finisched exercize 2

---
+++ exer2/exer2.js
@@ -0,0 +1,5 @@
+/* Declare an array named subjects containing strings.
+Declare a tuple named student that contains a string (for the name) and a number (for the age). */
+var subjects = ['john', 'dave', 'gue'];
+var student;
+student = ['john', 8];
---
+++ exer2/exer2.ts
@@ -0,0 +1,6 @@
+/* Declare an array named subjects containing strings.
+Declare a tuple named student that contains a string (for the name) and a number (for the age). */
+
+let subjects:string[]=['john','dave','gue']
+let student:[string,number]
+student=['john',8]
