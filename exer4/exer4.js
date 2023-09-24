de13a0f9bd7cfd1d2ac95a6247e3c2adb80d3a14
mcr45 <depr3604@protonmail.com>
2023-09-24 11:37:20 -0500

    exercize 4 completed

--- exer4/exer4.js
+++ exer4/exer4.js
@@ -1,6 +1,4 @@
 /* Write a function greet that takes in a name (string) and returns a greeting (string). */
-
-function greet(name:string):string{
-    return `hey ${name}, howsitgoing?
-    `
+function greet(name) {
+    return "hey ".concat(name, ", howsitgoing?\n    ");
 }
