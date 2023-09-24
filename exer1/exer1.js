23ff7b98a23da7ab3b0c90a53b0c567a92ef2a77
mcr45 <depr3604@protonmail.com>
2023-09-24 11:08:21 -0500

    first typescript exercize done

---
+++ exer1/exer1.js
@@ -0,0 +1,7 @@
+/* Declare a variable studentName of type string and assign it your name.
+Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+Log these variables to the console.
+ */
+var studentName = 'Dwn';
+var totalCourses = 10;
+console.log("".concat(studentName, " is taking ").concat(totalCourses, " courses"));
---
+++ exer1/exer1.ts
@@ -0,0 +1,8 @@
+/* Declare a variable studentName of type string and assign it your name.
+Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+Log these variables to the console.
+ */
+
+let studentName:string='Dwn'
+let totalCourses:number=10
+console.log(`${studentName} is taking ${totalCourses} courses`)
